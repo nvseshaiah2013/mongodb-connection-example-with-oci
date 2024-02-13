4f3f5b8b8bdfd2c7eee48e78cdad0e502b321b40
Venkata Seshaiah <nvseshaiah2013@gmail.com>
2024-02-13 23:14:13 +0530

    Project initial setup

--- src/test/java/com/example/mongodb/oci/connection/ConnectionApplicationTests.java
+++ src/test/java/com/example/mongodb/oci/connection/ConnectionApplicationTests.java
@@ -1,12 +1,6 @@
 package com.example.mongodb.oci.connection;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class ConnectionApplicationTests {
-
-	@Test
 	void contextLoads() {
 	}
 
